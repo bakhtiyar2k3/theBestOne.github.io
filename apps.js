a872ef5282c2e13ccb5ba046a567fcebcebf6984
bakhtiyar2k3 <140269216+bakhtiyar2k3@users.noreply.github.com>
2023-07-22 23:11:55 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,28 @@
+let menuBtn = document.querySelector(".menuBtn img");
+let navbarDropDown = document.querySelector(".navbar");
+let navbarLinks = document.querySelector(".navbar ul");
+let Link1 = document.querySelector(".link1");
+let Link2 = document.querySelector(".link2");
+let Link3 = document.querySelector(".link3");
+let Link4 = document.querySelector(".link4");
+menuBtn.addEventListener("click", function () {
+  navbarDropDown.classList.toggle("nav-active2");
+  navbarLinks.classList.toggle("nav-active");
+});
+Link1.addEventListener("click", function () {
+  navbarDropDown.classList.toggle("nav-active2");
+  navbarLinks.classList.toggle("nav-active");
+});
+Link2.addEventListener("click", function () {
+  navbarDropDown.classList.toggle("nav-active2");
+  navbarLinks.classList.toggle("nav-active");
+});
+Link3.addEventListener("click", function () {
+  navbarDropDown.classList.toggle("nav-active2");
+  navbarLinks.classList.toggle("nav-active");
+});
+Link4.addEventListener("click", function () {
+  navbarDropDown.classList.toggle("nav-active2");
+  navbarLinks.classList.toggle("nav-active");
+});s
+s
